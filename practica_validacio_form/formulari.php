9ca66504b1578883af92d58f86ca868a667050f0
unknown <adriagp90@gmail.com>
2024-02-25 13:18:37 +0100

    afegit fitxer php

---
+++ practica_validacio_form/formulari.php
@@ -0,0 +1,22 @@
+<?php
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $code = $_POST["code"];
+    $pass = $_POST["pass"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><?php echo $name ?></li>
+        <li><?php echo $name ?></li>
+        <li><?php echo $name ?></li>
+        <li><?php echo $name ?></li>
+    </ul>
+</body>
+</html>
