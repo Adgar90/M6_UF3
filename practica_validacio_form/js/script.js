97d64ee523544fad2414ddccaf428ed57307c876
unknown <adriagp90@gmail.com>
2024-02-25 12:45:54 +0100

    modificacions/alguns comments

--- practica_validacio_form/js/script.js
+++ practica_validacio_form/js/script.js
@@ -1,5 +1,7 @@
 //Selector d'elements
 let inputs = document.querySelectorAll("input");
+let inputPass = document.getElementById("inputPass");
+let inputPassEqual = document.getElementById("inputPassEqual");
 let spans = document.querySelectorAll("span");
 let ul = document.querySelector("ul");
 let liCollection = document.querySelectorAll("li");
@@ -10,9 +12,7 @@ let lowerCaseLetters = /[a-z]/;
 let upperCaseLetters = /[A-Z]/;
 let numbers = /[0-9]/;
 let specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-
-
-
+//Esdeveniments
 inputs.forEach((input, index) => {
     input.addEventListener("focusout", () => {
         switch (input.id) {
@@ -28,10 +28,6 @@ inputs.forEach((input, index) => {
         }
     });
 });
-
-let inputPass = document.getElementById("inputPass");
-let inputPassEqual = document.getElementById("inputPassEqual");
-
 inputPass.addEventListener("input", () => {
     validatePassword(inputPass);
     equalPassword(inputPass, inputPassEqual);
@@ -39,6 +35,7 @@ inputPass.addEventListener("input", () => {
 inputPassEqual.addEventListener("input", () => {
     equalPassword(inputPass, inputPassEqual);
 })
+//Funcions
 function checkInputValue(input, index) {
     if (input.value == "") {
         input.style.borderColor = "red";
@@ -58,7 +55,7 @@ function validateEmail(input, index) {
     }
 }
 function validatePassword(input) {
-    input.value.length > 7 ? liCollection[0].style.color = "green" : liCollection[0].style.color = "red";
+    input.value.length > 7 && input.value.length < 16 ? liCollection[0].style.color = "green" : liCollection[0].style.color = "red";
     upperCaseLetters.test(input.value) ? liCollection[1].style.color = "green" : liCollection[1].style.color = "red";
     lowerCaseLetters.test(input.value) ? liCollection[2].style.color = "green" : liCollection[2].style.color = "red";
     numbers.test(input.value) ? liCollection[3].style.color = "green" : liCollection[3].style.color = "red";
@@ -73,7 +70,7 @@ function validatePassword(input) {
     
 }
 function acceptablePassword(password) {
-    if (password.length > 7 && upperCaseLetters.test(password) && lowerCaseLetters.test(password) && numbers.test(password) && specialChars.test(password)) {
+    if ((password.length > 7 && password.length < 16) && upperCaseLetters.test(password) && lowerCaseLetters.test(password) && numbers.test(password) && specialChars.test(password)) {
         return true;
     }
     return false;
